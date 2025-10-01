9389549d8a9d5ec5d644390750f19077fb5fd3fc
jordan24cyber <152842413+jordan24cyber@users.noreply.github.com>
2025-10-01 22:28:42 +0700

    update tes.php

---
+++ tes.php
@@ -0,0 +1,12 @@
+<?php
+$Transportasi= 1000000;
+$Konsumsi= 5000000;
+$Penginapan= 800000;
+$Jumlah= 3;
+
+$total= $Jumlah*($Transportasi+$Konsumsi+$Penginapan);
+
+echo " Total Pembayaran $Jumlah orang: $total";
+
+
+?>
